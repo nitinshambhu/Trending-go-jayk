ef91bd93aa01ff12d897343588d5f2beb34912ce
Nitin <nitinshambhu@hotmail.com>
2020-09-12 16:34:53 +0100

    [New Changes] : Introduced coroutines in Repository. Still fetching data from static data. It works

--- app/src/main/java/com/githubrepos/trending/repos/data/repository/RepositoriesRepository.kt
+++ app/src/main/java/com/githubrepos/trending/repos/data/repository/RepositoriesRepository.kt
@@ -4,17 +4,28 @@ import com.githubrepos.trending.common.DataFetchType
 import com.githubrepos.trending.repos.api.RepositoriesApi
 import com.githubrepos.trending.repos.data.Repository
 import com.githubrepos.trending.repos.data.staticRepositoriesData
+import com.githubrepos.trending.util.logD
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.lang.reflect.Type
 
 class RepositoriesRepository(val repoApi: RepositoriesApi) {
 
-    fun getRepositories(fetchType: DataFetchType = DataFetchType.Force): List<Repository> {
-        return fetchFromStaticData()
+    suspend fun getRepositories(fetchType: DataFetchType = DataFetchType.Force): List<Repository> {
+        return withContext(Dispatchers.Default){
+            return@withContext fetchFromStaticData()
+        }
     }
 
-    fun fetchFromStaticData(): List<Repository> {
+    suspend fun fetchFromRemote() : List<Repository> {
+        val repositoryList = repoApi.getRepositories()
+        "repositoryList received size = ${repositoryList.size}".logD("Test===")
+        return repositoryList
+    }
+
+    suspend fun fetchFromStaticData(): List<Repository> {
         val collectionType: Type = object : TypeToken<List<Repository>>() {}.type
         return Gson().fromJson(staticRepositoriesData, collectionType)
     }
