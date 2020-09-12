a1cdff2c59a8a89c49763f08ef9c36e0470bb222
Nitin <nitinshambhu@hotmail.com>
2020-09-12 21:47:54 +0100

    [Fix / Refactor] : Compilation error fixed. Added a method that will me write test cases

--- app/src/main/java/com/githubrepos/trending/common/CacheSession.kt
+++ app/src/main/java/com/githubrepos/trending/common/CacheSession.kt
@@ -3,25 +3,21 @@ package com.githubrepos.trending.common
 import android.content.SharedPreferences
 import android.os.SystemClock
 import androidx.core.content.edit
-import com.githubrepos.trending.util.logD
 import java.util.concurrent.TimeUnit
 
 class CacheSession(private val preferences: SharedPreferences) {
 
     fun startFor(feature: Feature) {
         preferences.edit(commit = true) {
-            putLong(feature.name, SystemClock.elapsedRealtime())
+            putLong(feature.name, getCurrentTime())
         }
     }
 
     fun isTimedOutFor(feature: Feature): Boolean {
         val savedTimestamp = preferences.getLong(feature.name, 0L)
-        val timeoutConfigured = TimeUnit.MINUTES.toMillis(feature.timeOutInMinutes)
-        val currentTimestamp = SystemClock.elapsedRealtime()
-        "savedTimestamp ==== $savedTimestamp".logD("Test===")
-        "currentTimestamp ==== $currentTimestamp".logD("Test===")
-        "timeoutConfigured ==== $timeoutConfigured".logD("Test===")
-        "Difference in time ==== ${currentTimestamp - savedTimestamp}".logD("Test===")
-        return (currentTimestamp - savedTimestamp) >= timeoutConfigured
+        val timeoutConfigured = TimeUnit.HOURS.toMillis(feature.timeOutInMinutes.toLong())
+        return (getCurrentTime() - savedTimestamp) >= timeoutConfigured
     }
+
+    fun getCurrentTime() = SystemClock.elapsedRealtime()
 }
