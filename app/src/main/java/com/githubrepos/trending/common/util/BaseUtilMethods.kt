136a8a2b7e9c7ca1e4aa88c26cd28c18987db99b
Nitin <nitinshambhu@hotmail.com>
2020-09-13 14:00:28 +0100

    [Tests] : Added test cases for BaseUtilMethods and also introduced library to handle suspend functions

--- app/src/main/java/com/githubrepos/trending/common/util/BaseUtilMethods.kt
+++ app/src/main/java/com/githubrepos/trending/common/util/BaseUtilMethods.kt
@@ -2,13 +2,25 @@ package com.githubrepos.trending.common.util
 
 import com.githubrepos.trending.common.data.ApiResponse
 
+/**
+ *  Converts any non-null object to an object of type ApiResponse.Success
+ */
 fun <T> T.asSuccess(): ApiResponse<T> = ApiResponse.Success(this)
 
+/**
+ *  Converts any exception to an object of type ApiResponse.Error
+ *  Typically we could logs these errors on a remote logging server.
+ *  For sake of this test, I am only printing the stacktrace so that the error appears in logs
+ */
 fun <T> Exception.asError(): ApiResponse<T> {
     printStackTrace()
     return ApiResponse.Error(this.message!!)
 }
 
+/**
+ *  This method takes a suspend function that returns data & converts the data to ApiResponse format
+ *  This method also handles any exceptions thrown to let ViewModels interpret the Api response gracefully
+ */
 suspend fun <T> apiResponseFrom(block: suspend () -> T): ApiResponse<T> {
     return try {
         block.invoke().asSuccess()
@@ -17,6 +29,10 @@ suspend fun <T> apiResponseFrom(block: suspend () -> T): ApiResponse<T> {
     }
 }
 
+/**
+ *  This method takes accepts ApiResponse class type & allows devs to avoid writing repetitive
+ *  when clause. Instead, they can focus on logic than boiler plate code.
+ */
 fun <T> apiResponseHandler(
     status: ApiResponse<T>,
     onSuccess: (T) -> Unit,
---
+++ app/src/test/java/com/githubrepos/trending/common/util/BaseUtilMethodsTest.kt
@@ -0,0 +1,78 @@
+package com.githubrepos.trending.common.util
+
+import com.githubrepos.trending.common.BaseTest
+import com.githubrepos.trending.common.data.ApiResponse
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class BaseUtilMethodsTest : BaseTest() {
+
+    @MockK(relaxUnitFun = true)
+    lateinit var randomClass: RandomClass
+
+    @Test
+    fun `apiResponseFrom method should return ApiResponse Success on executing a method without exceptions`() {
+        val list = listOf<Int>(1)
+        coEvery { randomClass.performAction() } returns list
+        runBlockingTest {
+            var apiResponse = apiResponseFrom { randomClass.performAction() }
+            assertTrue(apiResponse is ApiResponse.Success)
+            assertEquals((apiResponse as ApiResponse.Success).data, list)
+        }
+    }
+
+    @Test
+    fun `apiResponseFrom method should return ApiResponse Error on executing a method that throws exceptions`() {
+
+        coEvery { randomClass.performAction() } throws RuntimeException("Test")
+
+        runBlockingTest {
+            var apiResponse = apiResponseFrom { randomClass.performAction() }
+            assertTrue(apiResponse is ApiResponse.Error)
+            assertEquals((apiResponse as ApiResponse.Error).message, "Test")
+        }
+    }
+
+    @Test
+    fun `apiResponseHandler method should execute success block when ApiResponse is Success`() {
+
+        val apiResponse = listOf<Int>().asSuccess()
+
+        apiResponseHandler(
+            status = apiResponse,
+            onSuccess = { randomClass.onSuccess() },
+            onError = { randomClass.onError() }
+        )
+
+        assertTrue(apiResponse is ApiResponse.Success)
+        verify(exactly = 1) { randomClass.onSuccess() }
+        verify(exactly = 0) { randomClass.onError() }
+    }
+
+    @Test
+    fun `apiResponseHandler method should execute error block when ApiResponse is Error`() {
+
+        val apiResponse = Exception("Test").asError<List<Int>>()
+
+        apiResponseHandler(
+            status = apiResponse,
+            onSuccess = { randomClass.onSuccess() },
+            onError = { randomClass.onError() }
+        )
+
+        assertTrue(apiResponse is ApiResponse.Error)
+        verify(exactly = 0) { randomClass.onSuccess() }
+        verify(exactly = 1) { randomClass.onError() }
+    }
+}
+
+interface RandomClass {
+    suspend fun performAction(): List<Int>
+    fun onSuccess()
+    fun onError()
+}
